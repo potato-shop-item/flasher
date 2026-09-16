9b6dd398a4cc9c280cee993778d39d85562ff3ef
potato-ptt <mirinsio50@gmail.com>
2026-09-17 01:11:55 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ import {
   FlashSizeValues,
   LoaderOptions,
   Transport,
-} from "../../../lib";
+} from "../lib";
 import { serial } from "web-serial-polyfill";
 
 const serialLib = !navigator.serial && navigator.usb ? serial : navigator.serial;
